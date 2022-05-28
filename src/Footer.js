5b3c67290c154b9f7b6ea39ddc1a45356924a719
abg803 <98655951+abg803@users.noreply.github.com>
2022-05-28 17:08:28 -0400

    created footer component

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <p>
+        <a href="https://github.com/abg803/weather-app-new" target="_blank">
+            Open-source code
+        </a>
+        {" "}and design by{" "}
+        <a href="https://www.allegrabrooks.com/" target="_blank">
+            Allegra Brooks Gurfein
+        </a>
+      </p>
+    )
+
+}
+export default Footer;
