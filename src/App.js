cbbb3af5315422e42458d57379a89428b198b60c
abg803 <98655951+abg803@users.noreply.github.com>
2022-05-28 22:45:49 -0400

    created foundation files for components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
 import Footer from './Footer';
+import CurrentLocation from './CurrentLocation';
+import SevenDayForecast from './SevenDayForecast';
+import Search from './Search';
 
 function App() {
   const [data, setData] = useState(null);
@@ -16,18 +19,20 @@ function App() {
     
     axios
       .get(apiUrl)
-      
       .then(response => {
         // 3. set the data to a state
         setData(response)
-
-
       });
-
   }, [])
 
   return (
     <div className="App">
+
+      <Search/>
+      <CurrentLocation/>
+      <SevenDayForecast/>
+
+
     
       <pre>
        {JSON.stringify(data, null, 2)} 
---
+++ src/SevenDayForecast.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function SevenDayForecast (){
+    return(
+        <div>hello</div>
+    )
+};
+export default SevenDayForecast;
